a8bdffd3d2ab74e33a601903d83033f08ac5f7db
Arnaud Schaeffer <arnaud.schaeffer@its-future.com>
2024-08-21 10:51:29 +0200

    Remove hidden sensio dependency

--- Controller/JobController.php
+++ Controller/JobController.php
@@ -7,17 +7,15 @@ use Doctrine\ORM\EntityManager;
 use JMS\JobQueueBundle\Entity\Job;
 use JMS\JobQueueBundle\Entity\Repository\JobManager;
 use JMS\JobQueueBundle\View\JobFilter;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\Routing\Attribute\Route;
 
 class JobController extends AbstractController
 {
-    /**
-     * @Route("/", name = "jms_jobs_overview")
-     */
+    #[Route(path: '/', name: 'jms_jobs_overview')]
     public function overviewAction(Request $request)
     {
         $jobFilter = JobFilter::fromRequest($request);
@@ -63,9 +61,7 @@ class JobController extends AbstractController
         ));
     }
 
-    /**
-     * @Route("/{id}", name = "jms_jobs_details")
-     */
+    #[\Symfony\Component\Routing\Annotation\Route(path: '/{id}', name: 'jms_jobs_details')]
     public function detailsAction(Job $job)
     {
         $relatedEntities = array();
@@ -124,9 +120,7 @@ class JobController extends AbstractController
         ));
     }
 
-    /**
-     * @Route("/{id}/retry", name = "jms_jobs_retry_job")
-     */
+    #[\Symfony\Component\Routing\Attribute\Route(path: '/{id}/retry', name: 'jms_jobs_retry_job')]
     public function retryJobAction(Job $job)
     {
         $state = $job->getState();
